1a268c800f5c9e1203768f7476e36c22ec3426d8
Aleksandr Bukhalo <a@bukhalo.com>
2020-04-26 13:43:12 +0300

    feat: try function with payload

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { HelloNameDto } from './dto/hello-name.dto';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getHelloName(helloNameDto: HelloNameDto): string {
+    return `Hello ${helloNameDto.name}!`;
   }
 }
---
+++ src/dto/hello-name.dto.ts
@@ -0,0 +1,3 @@
+export class HelloNameDto {
+  name: string;
+}
